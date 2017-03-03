8b037be75d6aa5e41e5db76b303a320ea87d90d5
Joao Andre Sa <joao.gomesdesaesousa@epfl.ch>
2017-03-03 13:11:38 +0100

    Keep trying

--- simul/simul_test.go
+++ simul/simul_test.go
@@ -15,6 +15,7 @@ func TestSimulation(t *testing.T) {
 	/*simul.Start("runfiles/addrm_server.toml", "runfiles/collective_aggregation.toml", "runfiles/deterministic_tagging.toml", "runfiles/key_switching.toml",
 	"runfiles/local_aggregation.toml", "runfiles/local_clear_aggregation.toml", "runfiles/proofs_verification.toml", "runfiles/shuffling.toml", "runfiles/medco.toml")*/
 
-	simul.Start("runfiles/collective_aggregation.toml", "runfiles/deterministic_tagging.toml", "runfiles/key_switching.toml",
-		"runfiles/local_aggregation.toml", "runfiles/local_clear_aggregation.toml", "runfiles/proofs_verification.toml", "runfiles/shuffling.toml", "runfiles/medco.toml")
+	/*simul.Start("runfiles/collective_aggregation.toml", "runfiles/deterministic_tagging.toml", "runfiles/key_switching.toml",
+		"runfiles/local_aggregation.toml", "runfiles/local_clear_aggregation.toml", "runfiles/proofs_verification.toml", "runfiles/shuffling.toml", "runfiles/medco.toml")*/
+	simul.Start("runfiles/addrm_server.toml")
 }
