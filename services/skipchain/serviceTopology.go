949d81bdb71a68ecfd737ffa6d0f3a007dc01f11
Joao Andre Sa <joao.gomesdesaesousa@epfl.ch>
2017-05-10 11:19:01 +0200

    Started the cosi part

--- services/skipchain/serviceTopology.go
+++ services/skipchain/serviceTopology.go
@@ -8,6 +8,8 @@ import (
 	"gopkg.in/dedis/onet.v1/network"
 	"gopkg.in/dedis/onet.v1/log"
 	"github.com/JoaoAndreSa/MedCo/services"
+	"github.com/JoaoAndreSa/MedCo/protocols"
+	"github.com/dedis/cothority/cosi/protocol"
 )
 
 // ServiceName is the registered name for the skipchain topology service.
@@ -118,6 +120,7 @@ func (s *Service) Process(msg *network.Envelope) {
 
 // HandleTopologyCreationQuery handles the reception of a topology creation query.
 func (s *Service) HandleTopologyCreationQuery(recq *TopologyCreationQuery) (network.Message, onet.ClientError) {
+	log.LLvl1(s.ServerIdentity(), "received a request for a new topology skipchain")
 
 	if recq.IntraMessage==false{
 		recq.IntraMessage = true
@@ -127,10 +130,102 @@ func (s *Service) HandleTopologyCreationQuery(recq *TopologyCreationQuery) (netw
 			log.Error("broadcasting error ", err)
 		}
 
-		time.Sleep(5*time.Second)
-
 	}
+
+	survey := castToSurvey(s.Survey.Get((string)(targetSurvey)))
+
+	if len(survey.DpResponses) == 0 && len(survey.DpResponsesAggr) == 0 {
+		log.LLvl1(s.ServerIdentity(), " no data to shuffle")
+		return nil
+	}
+
+	pi, err := s.StartProtocol(protocols.ShufflingProtocolName, targetSurvey)
+	if err != nil {
+		return err
+	}
+	shufflingResult := <-pi.(*protocols.ShufflingProtocol).FeedbackChannel
+
+	survey.PushShuffledProcessResponses(shufflingResult)
+	s.Survey.Put(string(targetSurvey), survey)
+
+
+	return err
+
 
 	log.LLvl1(s.ServerIdentity(), "successfuly created a topology skipchain")
 	return &ServiceState{Error: nil}, nil
+}
+
+// Protocol Handlers
+//______________________________________________________________________________________________________________________
+
+// NewProtocol creates a protocol instance executed by all nodes
+func (s *Service) NewProtocol(tn *onet.TreeNodeInstance, conf *onet.GenericConfig) (onet.ProtocolInstance, error) {
+	if tn.ProtocolName() != cosi.Name {
+		log.Fatal("Shouldn't you be calling a cosi protocol")
+	}
+
+	tn.SetConfig(conf)
+
+	pi, err := cosi.NewProtocol(tn)
+
+	done := make(chan bool)
+	// create the message we want to sign for this round
+	msg := []byte("Hello World Cosi")
+
+	// Register the function generating the protocol instance
+	var root *cosi.CoSi
+	// function that will be called when protocol is finished by the root
+	doneFunc := func(sig []byte) {
+		suite := hosts[0].Suite()
+		publics := el.Publics()
+		if err := root.VerifyResponses(aggPublic); err != nil {
+			t.Fatal("Error verifying responses", err)
+		}
+		if err := VerifySignature(suite, publics, msg, sig); err != nil {
+			t.Fatal("Error verifying signature:", err)
+		}
+		done <- true
+	}
+
+	// Start the protocol
+	p, err := local.CreateProtocol("CoSi", tree)
+	if err != nil {
+		t.Fatal("Couldn't create new node:", err)
+	}
+	root = p.(*CoSi)
+	root.Message = msg
+	responseFunc := func(in []abstract.Scalar) {
+		log.Lvl1("Got response")
+		if len(root.Children()) != len(in) {
+			t.Fatal("Didn't get same number of responses")
+		}
+	}
+	root.RegisterResponseHook(responseFunc)
+	root.RegisterSignatureHook(doneFunc)
+
+
+	return pi, nil
+}
+
+// StartProtocol starts a specific protocol (Pipeline, Shuffling, etc.)
+func (s *Service) StartProtocol(name string) (onet.ProtocolInstance, error) {
+	tmp := castToSurvey(s.Survey.Get((string)(targetSurvey)))
+	tree := tmp.Query.Roster.GenerateNaryTreeWithRoot(2, s.ServerIdentity())
+
+	var tn *onet.TreeNodeInstance
+	tn = s.NewTreeNodeInstance(tree, tree.Root, name)
+
+	conf := onet.GenericConfig{Data: []byte(string(targetSurvey))}
+
+	pi, err := s.NewProtocol(tn, &conf)
+	if err != nil {
+		log.Fatal("Error running" + name)
+	}
+
+	s.RegisterProtocolInstance(pi)
+	go pi.Dispatch()
+	go pi.Start()
+
+	return pi, err
 }
