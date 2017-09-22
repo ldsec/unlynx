4705c67c2480fe77f24bcbf2251902122a7d5e4f
Mickaël Misbach <mickael@misba.ch>
2017-09-22 09:38:28 +0200

    install script modifications

--- app/i2b2/install_script.sh
+++ app/i2b2/install_script.sh
@@ -1,28 +1,21 @@
 #!/usr/bin/env bash
 
-apt-get update
-
-yes Y | apt-get install git
-
-apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common \
-    screen
+apt-get -y update
+apt-get -y install git apt-transport-https ca-certificates curl software-properties-common screen
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 apt-key fingerprint 0EBFCD88
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
 
-apt-get update
-
-yes Y | apt-get install docker-ce
+apt-get -y update
+apt-get -y install docker-ce postgresql-client-9.6
 
 docker run hello-world
 
@@ -30,13 +23,4 @@ curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compos
 
 chmod +x /usr/local/bin/docker-compose
 
-sudo add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
-
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-
-sudo apt-get update
-
-sudo apt-get install postgresql-client-9.6
-
 #git clone https://c4science.ch/source/medco-deployment.git
- 
