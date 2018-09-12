3e0ab09df8fbc0e592899b69971932cfb1cc4ef3
David Froelicher <david.froelicher@epfl.ch>
2018-09-12 16:48:50 +0200

    need to be able to change suite

--- lib/suite.go
+++ lib/suite.go
@@ -1,18 +1,21 @@
 package libunlynx
 
 import (
-	"github.com/dedis/kyber"
-	"github.com/dedis/kyber/suites"
+	"github.com/dedis/kyber/pairing/bn256"
+	"github.com/dedis/cothority"
 )
 
-// Suite defines the capabilities required by the lib package.
-type Suite interface {
-	kyber.Group
-	kyber.Random
-	kyber.HashFactory
-	kyber.XOF
-	kyber.XOFFactory
+// SuiTe is the instantiation of the suite
+var SuiTe = bn256.NewSuiteG1()
+
+func init() {
+	cothority.Suite=SuiTe
 }
 
-// SuiTe defines the Suite type/ curve (Ed25519, or other)
-var SuiTe = suites.MustFind("Ed25519") // Use the edwards25519-curve
+// SuiTe in this case is the ed25519 curve
+//var SuiTe = suites.MustFind("Ed25519")
+//var SuiTe = suites.MustFind("bn256.g1")
+
+func CurvePairingTest() bool {
+	return SuiTe.String() == "combined:bn256.G1"
+}
