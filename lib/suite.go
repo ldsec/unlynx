a7fb6e827ae7bbe4d79b8363f351cacebd8e28f9
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-09-19 13:15:21 +0200

    Changed suite

--- lib/suite.go
+++ lib/suite.go
@@ -1,16 +1,15 @@
 package libunlynx
 
 import (
-	"github.com/dedis/cothority"
 	"github.com/dedis/kyber/suites"
 )
 
 // SuiTe is the instantiation of the suite
 // var SuiTe = bn256.NewSuiteG1()
 
-func init() {
-	cothority.Suite = SuiTe
-}
+//func init() {
+//	cothority.Suite = SuiTe
+//}
 
 // SuiTe in this case is the ed25519 curve
 var SuiTe = suites.MustFind("Ed25519")
