238f95e01ff4c2175707473a19970edea0041118
JoaoAndreSa <joao.gomesdesaesousa@epfl.ch>
2018-09-19 12:59:24 +0200

    Changed suite

--- lib/suite.go
+++ lib/suite.go
@@ -1,21 +1,22 @@
 package libunlynx
 
 import (
-	"github.com/dedis/kyber/pairing/bn256"
 	"github.com/dedis/cothority"
+	"github.com/dedis/kyber/suites"
 )
 
 // SuiTe is the instantiation of the suite
-var SuiTe = bn256.NewSuiteG1()
+// var SuiTe = bn256.NewSuiteG1()
 
 func init() {
-	cothority.Suite=SuiTe
+	cothority.Suite = SuiTe
 }
 
 // SuiTe in this case is the ed25519 curve
-//var SuiTe = suites.MustFind("Ed25519")
+var SuiTe = suites.MustFind("Ed25519")
+
 //var SuiTe = suites.MustFind("bn256.g1")
 
-func CurvePairingTest() bool {
-	return SuiTe.String() == "combined:bn256.G1"
-}
+//func CurvePairingTest() bool {
+//	return SuiTe.String() == "combined:bn256.G1"
+//}
