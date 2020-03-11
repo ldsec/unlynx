32819216dd9db948a71b7142eb72161c4c087c17
Joao Sa Sousa <jagsousa1992@gmail.com>
2020-03-11 10:32:33 +0100

    Re-added simulation tests

--- simul/simul_test.go
+++ simul/simul_test.go
@@ -12,5 +12,6 @@ func TestMain(m *testing.M) {
 }
 
 func TestSimulation(t *testing.T) {
-	simul.Start("runfiles/addrm_server.toml")
+	simul.Start("runfiles/addrm_server.toml", "runfiles/collective_aggregation.toml", "runfiles/deterministic_tagging.toml", "runfiles/key_switching.toml",
+		"runfiles/local_aggregation.toml", "runfiles/local_clear_aggregation.toml", "runfiles/proofs_verification.toml", "runfiles/shuffling.toml", "runfiles/shuffling+ddt.toml", "runfiles/unlynx_default.toml")
 }
